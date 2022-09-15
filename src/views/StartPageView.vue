179a17ff24b53011dcd1b6efd630540f5c627a48
nitslaszlo <nitslaszlo@gmail.com>
2022-09-15 10:01:49 +0200

    up

--- src/views/StartPageView.vue
+++ src/views/StartPageView.vue
@@ -1,36 +1,7 @@
-<script setup lang="ts">
-  let counter1 = ref(123);
-  let state = reactive({
-    counter2: 0,
-  });
-
-  const titleClass = ref("titleRed");
-
-  function inc(): void {
-    counter1.value++;
-    state.counter2++;
-    if (state.counter2 % 2 == 0) {
-      titleClass.value = "titleRed";
-    } else {
-      titleClass.value = "titleBlue";
-    }
-  }
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <q-page>
-    <q-btn color="red" label="Increment" no-caps text-color="black" @click="inc()" />
-    <h1>{{ counter1 }}</h1>
-    <h1>{{ state.counter2 }}</h1>
-    <h1 :class="titleClass">Make me red</h1>
-  </q-page>
+  <q-page></q-page>
 </template>
 
-<style>
-  .titleRed {
-    color: red;
-  }
-  .titleBlue {
-    color: blue;
-  }
-</style>
+<style lang="scss" scoped></style>
